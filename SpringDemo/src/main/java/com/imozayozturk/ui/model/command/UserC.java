ad570e030b7af274b0a5cafcf9e53df69863eff1
ozay <ozturkozay1907@hotmail.com>
2019-03-11 23:42:39 +0300

    Command-Query models are provided.

---
+++ SpringDemo/src/main/java/com/imozayozturk/ui/model/command/UserC.java
@@ -0,0 +1,21 @@
+package com.imozayozturk.ui.model.command;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class UserC {
+
+	@Getter
+	@Setter
+	private String name;
+	@Getter
+	@Setter
+	private String surname;
+	@Getter
+	@Setter
+	private String email;
+	@Getter
+	@Setter
+	private String password;
+
+}
---
+++ SpringDemo/src/main/java/com/imozayozturk/ui/model/query/UserQ.java
@@ -0,0 +1,20 @@
+package com.imozayozturk.ui.model.query;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class UserQ {
+
+	@Getter
+	@Setter
+	private String userId;
+	@Getter
+	@Setter
+	private String name;
+	@Getter
+	@Setter
+	private String surname;
+	@Getter
+	@Setter
+	private String email;
+}
